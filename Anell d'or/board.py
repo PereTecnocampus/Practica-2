0b4a7786afdc793c592af937d127d3e237039157
LlunaTecnocampus <165902621+LlunaTecnocampus@users.noreply.github.com>
2026-05-18 11:55:14 +0200

    Board done

---
+++ Anell d'or/board.py
@@ -0,0 +1,80 @@
+import numpy as np
+from enum import Enum
+import random
+
+WIDTH = 12
+HEIGHT = 12
+
+class PieceType(Enum):
+    EMPTY = 0
+    WALL = 1
+    BRONZE = 2
+    SILVER = 3
+    GOLD = 4
+
+class Board():
+    def __init__(self):
+        self.board = np.zeros((WIDTH, HEIGHT), dtype=int)
+
+    def _can_place_wall(self, pos, length, direction):
+        for i in range(length):
+            target = pos + i * direction
+
+            if target[0] >= WIDTH or target[1] >= HEIGHT or target[0] < 0 or target[1] < 0:
+                return False
+
+            if self.board[target[1], target[0]] != PieceType.EMPTY.value:
+                return False
+            
+        return True
+
+    def _populate_walls(self):
+        wall_lengths = [5, 4, 3, 3, 3]
+        for length in wall_lengths:
+
+            can_place = False
+            while not can_place:
+                pos = np.array([random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)])
+
+                dir = np.array([0, 0])
+                while np.all(dir == 0):
+                    dir = np.array([random.randint(-1, 1), random.randint(-1, 1)])
+
+                can_place = self._can_place_wall(pos, length, dir)
+            
+            for i in range(length):
+                target = pos + i * dir
+                self.board[target[1], target[0]] = PieceType.WALL.value
+
+    def _populate_coins(self):
+        coins = [6, 3, 1]
+        coin_types = [PieceType.BRONZE, PieceType.SILVER, PieceType.GOLD]
+
+        for i, coin_type in enumerate(coin_types):
+            for j in range(coins[i]):
+                while True:
+                    pos = np.array([random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)])
+                    if self.board[pos[1], pos[0]] == PieceType.EMPTY.value:
+                        self.board[pos[1], pos[0]] = coin_type.value
+                        break
+
+    def populate(self):
+        self._populate_walls()
+        self._populate_coins()
+
+    def get_valid_spawn_pos(self):
+        pos = None
+
+        while True:
+            pos = np.array([random.randint(0, WIDTH - 1), random.randint(0, HEIGHT - 1)])
+            if self.board[pos[1], pos[0]] == PieceType.EMPTY.value:
+                break
+
+        return pos
+
+if __name__ == "__main__":
+    board = Board()
+
+    board.populate()
+
+    print(board.board)
