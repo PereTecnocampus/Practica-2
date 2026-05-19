bfe6d0800e03048bdd94a21c96bc3681c86207be
zzniki <llunadgr@gmail.com>
2026-05-19 17:52:16 +0200

    Crear classe game

--- anell_dor/game.py
+++ anell_dor/game.py
@@ -6,6 +6,11 @@ class Game:
     def __init__(self):
         self.board = None
         self.player_pos = np.array([0, 0])
+        self.initial_best_route = None
+        self.best_route = None
+
+    def _update_best_route(self):
+        self.best_route = find_best_route(self.board, tuple(self.player_pos))
 
     def _init_board(self):
         self.board = Board()
@@ -17,10 +22,17 @@ class Game:
 
             raw_spawn = self.board.get_valid_spawn_pos()
             spawn_pos = (int(raw_spawn[0]), int(raw_spawn[1]))
+            self.player_pos = np.array(spawn_pos)
             
             result = find_best_route(self.board, spawn_pos)
 
         total_cost, full_path, sequence = result
+        self.best_route = (total_cost, full_path, sequence)
+        self.initial_best_route = self.best_route
 
     def start(self):
         self._init_board()
+
+    def move_player(self, direction: np.ndarray):
+        self.player_pos += direction
+        self._update_best_route()
